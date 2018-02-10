148d071e51b7f02825b3e0106760d9cb1a6c6175
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-10 18:00:32 +0000

    f
    f

--- commands/vote.js
+++ commands/vote.js
@@ -3,8 +3,8 @@ module.exports.run = async (bot, message, args) => {
   if(!message.member.roles.find("name", "staff")) return message.reply("you can't use vote command.");
   let vote = await message.channel.send(args.join(" "));
   if(!vote) return message.reply("you need to input a statement.");
-  await vote.react("☑");
-  await vote.react("❌");
+   vote.react("☑");
+   vote.react("❌");
 
 }
 module.exports.help = {
