3df53af06895c43baf10fa747e277cdaf938fd04
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-10 18:24:16 +0000

    f
    f

--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  message.channel.reply("i am up and running.");
+  message.reply("i am up and running.");
   message.delete().catch(O_o=>{});
 }
 module.exports.help = {
