a6f7bee177635bdbbf9bc6d161b7e899e5405d51
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-10 13:58:56 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -3,11 +3,12 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!kUser) return message.channel.send("Can't find user!");
-  if(!message.member.hasPermission("KICK_MEMBERS")) return message.channel.send("You don't have enough permissions.");
-  if(kUser.hasPermission("KICK_MEMBERS")) return message.channel.send("That person can't be kicked!");
+  if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You don't have enough permissions.");
+  if(kUser.hasPermission("BAN_MEMBERS")) return message.channel.send("That person can't be kicked!");
 
-  message.guild.member(kUser).kick();
-  message.channel.send(`${kUser} has been kicked by <@${message.author.id}>`)
+  message.guild.member(kUser).ban();
+  message.channel.send(`${kUser} has been banned by <@${message.author.id}>`)
+  kUser.sendMessage("You have been banned from DBOR Discord.");
   return;
 }
 
