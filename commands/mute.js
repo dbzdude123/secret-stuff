2ab47693c524ee5ac1dee99268183b7526586e34
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-10 17:33:03 +0000

    v
    v

--- commands/mute.js
+++ commands/mute.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args) => {
   let mUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.reply("you don't have enough permissions.");
     if(!mUser) return message.reply("Couldn't find user.");
-    if(mUser.hasPermission("MANAGE_ROLES")) return message.reply("Can't mute that user!");
+    if(mUser.hasPermission("MANAGE_ROLES")) return message.reply("can't mute that user!");
     if(mUser = "@Universe 2 Bot") return message.reply("nice try you twat!");
     let muterole = message.guild.roles.find(`name`, "Muted");
     if(!muterole){
