cb95573de9e37db25f292e8445a05afdea2e9e77
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-10 17:24:45 +0000

    Fix ban
    Fix ban reply

--- commands/ban.js
+++ commands/ban.js
@@ -4,7 +4,7 @@ module.exports.run = async (bot, message, args) => {
   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!kUser) return message.channel.send("Can't find user!");
   if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You don't have enough permissions.");
-  if(kUser.hasPermission("BAN_MEMBERS")) return message.channel.send("That person cannot be kicked!");
+  if(kUser.hasPermission("BAN_MEMBERS")) return message.channel.send("That person cannot be banned!");
 
   message.guild.member(kUser).ban();
   message.channel.send(`${kUser} has been banned by <@${message.author.id}>.`)
