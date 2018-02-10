0d918c6b7490527f78f7c94b6260a60fc795e139
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-10 17:40:15 +0000

    match
    match

--- commands/mute.js
+++ commands/mute.js
@@ -4,9 +4,8 @@ const ms = require("ms");
 module.exports.run = async (bot, message, args) => {
   let mUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!message.member.hasPermission("MANAGE_ROLES")) return message.channel.reply("you don't have enough permissions.");
-    if(!mUser) return message.reply("Couldn't find user.");
+    if(!mUser) return message.reply("couldn't find user.");
     if(mUser.hasPermission("MANAGE_ROLES")) return message.reply("can't mute that user!");
-    if(mUser.user = "@Universe 2 Bot") return message.reply("nice try you twat!");
     let muterole = message.guild.roles.find(`name`, "Muted");
     if(!muterole){
       try{
@@ -26,7 +25,7 @@ module.exports.run = async (bot, message, args) => {
       }
     }
     let mutetime = args[1];
-    if(!mutetime) return message.reply("You need to specify a time ammount for the mute.");
+    if(!mutetime) return message.reply("you need to specify a time ammount for the mute.");
 
     await(mUser.addRole(muterole.id));
     message.reply(`<@${mUser.id}> has been muted for ${ms(ms(mutetime))}`);
