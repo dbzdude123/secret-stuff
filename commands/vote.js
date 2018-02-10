162e45d74b8e65317c97f1216a296b0ed9c26664
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-10 18:03:15 +0000

    f
    f

--- commands/vote.js
+++ commands/vote.js
@@ -1,7 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   if(!message.member.roles.find("name", "staff")) return message.reply("you can't use vote command.");
-  if(!vote) return message.reply("you need to input a statement.");
   let vote =  message.channel.send(args.join(" "));
   vote.react("☑");
   vote.react("❌");
