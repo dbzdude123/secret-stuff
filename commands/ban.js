bace75365aca7a46dfb384810b5f825dcfceff5c
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-10 14:01:40 +0000

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -8,7 +8,7 @@ module.exports.run = async (bot, message, args) => {
 
   message.guild.member(kUser).ban();
   message.channel.send(`${kUser} has been banned by <@${message.author.id}>`)
-  kUser.sendMessage(`${kUser} you've been banned from ${message.guild.name} by ${message.author}`);
+  kUser.sendMessage(`${kUser} you've been banned from ${message.guild.name} by ${message.author}.`);
   return;
 }
 
