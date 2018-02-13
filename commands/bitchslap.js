1bd315f1d911110ebd9f5f967aa8224dedc41fee
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-13 19:04:40 +0000

    bb
    bb

---
+++ commands/bitchslap.js
@@ -0,0 +1,17 @@
+const Discord = require("discord.js");
+module.exports.run = async (bot, message, args) => {
+  let blUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!blUser) return message.channel.send("Can't find user!");
+  message.channel.sendMessage({
+         "embed": {
+                 title: 'You have been bitchslapped.',
+                 "image": {
+                 "url": "https://media.giphy.com/media/QlkiPtWYWRtgQ/source.gif",
+                 }
+             }
+         });
+
+}
+module.exports.help = {
+  name: ">check"
+}
