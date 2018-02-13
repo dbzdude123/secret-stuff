60ac98014b66e7497d57e19f9dbf60d313ca0c3d
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-13 19:09:53 +0000

    f
    f

--- commands/check.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js");
-module.exports.run = async (bot, message, args) => {
-  message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
-  message.delete().catch(O_o=>{});
-}
-module.exports.help = {
-  name: ">check"
-}
