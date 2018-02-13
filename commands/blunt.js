f0dfd470f7281d6a72f2a5c1ed295ae44d6fde3f
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-13 19:07:00 +0000

    p
    p

--- commands/blunt.js
+++ commands/blunt.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  let blUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-  if(!blUser) return message.channel.send("Can't find user!");
+  let blUseru = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
+  if(!blUseru) return message.channel.send("Can't find user!");
   message.channel.sendMessage({
          "embed": {
                  title: 'Here is your blunt sir/miss.',
--- commands/check.js
+++ commands/check.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  message.reply("i am up and running.");
+  message.channel.send(new Date().getTime() - message.createdTimestamp + " ms");
   message.delete().catch(O_o=>{});
 }
 module.exports.help = {
