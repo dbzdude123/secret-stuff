11953b6999fed9036405eeaf4aa98715c77a5a08
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-13 19:12:25 +0000

    f
    f

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  message.channel.send(client.ping + " ms");
+  message.channel.send(`Pong! \`${client.pings[0]}ms\``;
   message.delete().catch(O_o=>{});
 }
 module.exports.help = {
