c683e20ac9f74791b44032e027abf19bcadbd46e
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-18 14:18:03 +0000

    test

--- commands/powerlevel.js
+++ commands/powerlevel.js
@@ -1,9 +1,10 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let plUser = message.author
+  let idUser = message.author.id
   message.channel.send({embed: {
   color: Math.floor(Math.random() * 16777214) + 1,
-  description: `${plUser} ` + 'your power level is ' + Math.floor(Math.random() * 9677721412) * Math.floor(Math.random() * 27)
+  description: `${plUser} ` + 'your power level is ' + idUser * message.author.length
 }});
 
 }
