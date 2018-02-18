755ae599c7880def0b88e2a1c6a78798d091b03d
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-18 14:30:23 +0000

    f

--- commands/quote.js
+++ commands/quote.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  if(message.author.id !== "207438736575692800") return message.reply("you must have bot owner status to use this command.");
+  if(message.author.id !== "207438736575692800") return message.channel.send(`${message.author}, you can't use that command.`);
   message.channel.send(args.join(" "));
   message.delete();
 }
