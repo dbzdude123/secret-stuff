c810c7b28072ecc3bd9e78c6d8a1307315ea53ca
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-18 13:43:24 +0000

    t

--- commands/ping.js
+++ commands/ping.js
@@ -1,6 +1,10 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  message.reply('your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`');
+  let puser = message.guild.member(message.mentions.users.first()
+  message.channel.send({embed: {
+  color: 3447003,
+  description: `${puser }` + 'your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`'
+}});
 }
 module.exports.help = {
   name: ">ping"
