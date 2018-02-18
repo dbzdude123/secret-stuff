c17ec6eecafe3b868aa890c89efe47d7b2da0931
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-18 14:27:00 +0000

    fix

--- commands/quote.js
+++ commands/quote.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.roles.find("name", "staff")) return message.reply("you can't use quote command.");
+  if(msg.author.id !== "207438736575692800") return;
   message.channel.send(args.join(" "));
   message.delete();
 }
