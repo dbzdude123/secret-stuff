7fd8c3432d4a18a4fcd8887b978aba7d6897325a
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-18 13:51:22 +0000

    color

--- commands/ping.js
+++ commands/ping.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   let puser = message.author
   message.channel.send({embed: {
-  color: 669933,
+  color: Math.floor(Math.random() * 16777214) + 1,
   description: `${puser} ` + 'your ping is `' + `${Date.now() - message.createdTimestamp}` + ' ms`'
 }});
 }
