f11219515429b9a8102f1cfe91afeec251f00ce5
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-18 14:36:58 +0000

    f

--- commands/quote.js
+++ commands/quote.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  if(message.author.id !== "207438736575692800") return message.channel.send(`${message.author}, you can't use that command.`);
+  if(message.author.id !== "207438736575692800") return message.channel.send(`${message.author}, you must have bot owner stats.`);
   await message.delete();
   message.channel.send(args.join(" "));
 }
