5bba1e80281912c3da8cd9417a13ae901ac4865f
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-18 14:31:50 +0000

    test

--- commands/quote.js
+++ commands/quote.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   if(message.author.id !== "207438736575692800") return message.channel.send(`${message.author}, you can't use that command.`);
+  await message.delete();
   message.channel.send(args.join(" "));
-  message.delete();
 }
 module.exports.help = {
   name: ">quote"
