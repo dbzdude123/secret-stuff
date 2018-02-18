47b6734401ae9b48de25a2977cc74b345e0d0cc0
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-18 13:54:07 +0000

    fix

--- commands/purge.js
+++ commands/purge.js
@@ -1,8 +1,8 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("You can't purge!");
-  message.channel.fetchMessages({limit: args[0]}).then(messages => message.channel.bulkDelete(messages));
   message.delete();
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("you can't purge!");
+  message.channel.fetchMessages({limit: args[0]}).then(messages => message.channel.bulkDelete(messages));
 }
 module.exports.help = {
   name: ">purge"
