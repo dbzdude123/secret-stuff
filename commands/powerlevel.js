f5b5adbf203d17e5057a72bc457054ea09ac1ef6
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-19 21:26:20 +0000

    fix

--- commands/powerlevel.js
+++ commands/powerlevel.js
@@ -1,6 +1,9 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  if(message.author.id == "207438736575692800") return message.channel.send(`${message.author}, your power level is infinite (∞).`);
+  if(message.author.id == "207438736575692800") return message.channel.send({embed: {
+    color: Math.floor(Math.random() * 16777214) + 1,
+    description: `${message.author}, your power level is infinite (∞).`
+  }});
   let plUser = message.author
   let idUser = message.author.id
   message.channel.send({embed: {
