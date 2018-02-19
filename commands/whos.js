705d69ea669db39100777bc027bfbb71bcd21f8e
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-19 21:08:15 +0000

    test

--- commands/whos.js
+++ commands/whos.js
@@ -35,9 +35,9 @@ module.exports.run = async (bot, message, args) => {
   ];
   let wUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
   if(!wUser) return message.channel.send(`${message.author}, you haven't selected anyone.`);
-  if(message.author.id == "207438736575692800") return message.channel.send("Vida is the most awesome person in the world.");
-  if(message.author.id == "239035110987399178") return message.channel.send("Silent is the true goddess of Universe 2!");
-  if(message.author.id == "174558357363884041") return message.channel.send("Fearless is obviously gay, just look at him.");
+  if(wUser.id == "207438736575692800") return message.channel.send("Vida is the most awesome person in the world.");
+  if(wUser.id == "239035110987399178") return message.channel.send("Silent is the true goddess of Universe 2!");
+  if(wUser.id == "174558357363884041") return message.channel.send("Fearless is obviously gay, just look at him.");
   message.channel.send(`${wUser}  is ` + fortunejar[Math.floor(Math.random() * fortunejar.length)]);
 
 }
