6912102db2e52a92b89ab7afa3280775c769c58e
dbzdude123 <36242635+dbzdude123@users.noreply.github.com>
2018-02-24 16:03:22 +0000

    nodies

--- commands/purge.js
+++ commands/purge.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
   await message.delete();
-  await if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("you can't purge!");
+  if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("you can't purge!");
   message.channel.fetchMessages({limit: args[0]}).then(messages => message.channel.bulkDelete(messages));
 }
 module.exports.help = {
